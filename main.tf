6dcc98cdb7938679c98009e7a352c8a8d0c11893
Peter Spasov <petar.spasov@hashicorp.com>
2022-06-01 20:01:35 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,6 @@ resource "github_repository_file" "file" {
   content             = "# Terraform Test Repo \n The repo was created by HashiCorp Terraform"
   commit_message      = "Managed by Terraform"
   commit_author       = "Peter Spasov"
-  commit_email        = "petar.spasov@hashicorp.com"
+  commit_email        = "spasov@hashicorp.com"
   overwrite_on_create = true
 }
