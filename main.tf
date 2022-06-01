d3d89dab583b609a170c2153799bb4387a1a8d71
51r <petar.spasov@hashicorp.com>
2022-06-01 15:16:22 +0300

    modified the code

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "github_repository_file" "file" {
   repository          = github_repository.terraform.name
   branch              = "main"
   file                = "README.md"
-  content             = "# This repo is managed by Hashicorp Terraform"
+  content             = "# This repo is managed by HashiCorp Terraform"
   commit_message      = "Managed by Terraform"
   commit_author       = "Peter Spasov"
   commit_email        = "petar.spasov@hashicorp.com"
