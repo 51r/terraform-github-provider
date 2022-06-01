0b1bd756562f82b147c3a90051b00528d143367a
51r <petar.spasov@hashicorp.com>
2022-06-01 15:14:35 +0300

    added main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "github_repository" "terraform" {
+  name        = "terraform-test-repo"
+  description = "This is a repo created by Terraform"
+  auto_init   = true
+
+  visibility = "public"
+
+}
+
+
+resource "github_repository_file" "file" {
+  repository          = github_repository.terraform.name
+  branch              = "main"
+  file                = "README.md"
+  content             = "# This repo is managed by Hashicorp Terraform"
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Peter Spasov"
+  commit_email        = "petar.spasov@hashicorp.com"
+  overwrite_on_create = true
+}
